2f07429611584488862e7f277b8e6ccc4a925805
Mohd Ashraf <98030979+mohdashraf1234@users.noreply.github.com>
2025-08-24 01:57:42 +0530

    Add files via upload

---
+++ trees/MaxDepthBinaryTree.java
@@ -0,0 +1,34 @@
+package com.trees;
+
+public class MaxDepthBinaryTree {
+
+    // Recursive DFS solution
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+
+    public static void main(String[] args) {
+        /*
+               1
+              / \
+             2   3
+            / \
+           4   5
+        */
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        int depth = maxDepth(root);
+        System.out.println("Maximum Depth of Binary Tree: " + depth);
+    }
+}
---
+++ trees/TreeNode.java
@@ -0,0 +1,13 @@
+package com.trees;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+        left = null;
+        right = null;
+    }
+}
